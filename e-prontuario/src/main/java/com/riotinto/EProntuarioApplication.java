be918604a1b6bed1d47ec2de3d01e313545d0095
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-02-07 14:55:46 -0300

    Arquivos de configuração

---
+++ e-prontuario/src/main/java/com/riotinto/EProntuarioApplication.java
@@ -0,0 +1,13 @@
+package com.riotinto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EProntuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EProntuarioApplication.class, args);
+	}
+
+}
