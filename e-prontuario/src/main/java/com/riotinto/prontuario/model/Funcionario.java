e57616332c917762b73e1b15c7fbd63f1fe9e149
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-02-07 20:32:05 -0300

    Criação do Model de Funcionários

---
+++ e-prontuario/src/main/java/com/riotinto/prontuario/model/Funcionario.java
@@ -0,0 +1,91 @@
+package com.riotinto.prontuario.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "funcionarios")
+public class Funcionario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(max = 20)
+	private String nome;
+	
+	@NotNull
+	@Size(max = 20)
+	private String sobrenome;
+	
+	@NotNull
+	@Size(max = 10)
+	private String dataContratacao;
+	
+	@NotNull
+	@Size(max = 15)
+	private String tipo;
+	
+	@NotNull
+	@Size(max = 20)
+	private String nomeLocalDeTrabalho;
+
+	
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getDataContratacao() {
+		return dataContratacao;
+	}
+
+	public void setDataContratacao(String dataContratacao) {
+		this.dataContratacao = dataContratacao;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNomeLocalDeTrabalho() {
+		return nomeLocalDeTrabalho;
+	}
+
+	public void setNomeLocalDeTrabalho(String nomeLocalDeTrabalho) {
+		this.nomeLocalDeTrabalho = nomeLocalDeTrabalho;
+	}
+	
+	
+}
