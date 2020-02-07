796db7d50287622471142e8d412a02447eed02ec
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-02-07 19:35:44 -0300

    Adição do método PUT

--- e-prontuario/src/main/java/com/riotinto/prontuario/controller/ProntuarioController.java
+++ e-prontuario/src/main/java/com/riotinto/prontuario/controller/ProntuarioController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -56,5 +57,13 @@ public class ProntuarioController {
 		
 		return prontuarios.save(prontuario);
 	}
+	
+	@PutMapping
+	@ResponseStatus(HttpStatus.OK)
+	public Prontuario atualizar(@Valid @RequestBody Prontuario prontuario) {
+		return prontuarios.save(prontuario);
+	}
+	
+	
 	
 }
