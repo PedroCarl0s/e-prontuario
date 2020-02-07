a772ce4d41413901636dae24f5aeca0e6b547f80
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-02-07 14:48:14 -0300

    Adição do método POST na rota /pacientes

--- e-prontuario/src/main/java/com/riotinto/prontuario/controller/PacienteController.java
+++ e-prontuario/src/main/java/com/riotinto/prontuario/controller/PacienteController.java
@@ -3,13 +3,19 @@ package com.riotinto.prontuario.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.riotinto.prontuario.model.Paciente;
 import com.riotinto.prontuario.repository.PacienteRepository;
@@ -35,6 +41,19 @@ public class PacienteController {
 		}
 		
 		return ResponseEntity.ok(paciente.get());
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Paciente adicionar(@Valid @RequestBody Paciente paciente) {
+		Optional<Paciente> pacienteExistente = pacientes.findByNomeAndSobrenomeAndDataNascimento
+				(paciente.getNome(), paciente.getSobrenome(), paciente.getDataNascimento());
+		
+		if (pacienteExistente.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Já existe um paciente com o mesmo nome, sobrenome e data de nascimento!");
+		}
+		
+		return pacientes.save(paciente);
 	}
 	
 	
