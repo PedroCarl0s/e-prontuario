c2ce80a6d510e954126660c9fa7191f925ec05c9
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-02-07 21:09:24 -0300

    Criação do CREATE TABLE FUNCIONARIOS

--- e-prontuario/src/main/resources/db/migration/V01__novas-tabelas.sql
+++ e-prontuario/src/main/resources/db/migration/V01__novas-tabelas.sql
@@ -26,3 +26,15 @@ CREATE TABLE IF NOT EXISTS prontuarios (
 	
 	PRIMARY KEY(id)
 );
+
+CREATE TABLE IF NOT EXISTS funcionarios(
+	id BIGINT auto_increment NOT NULL,
+	nome VARCHAR(20) NOT NULL,
+	sobrenome VARCHAR(20) NOT NULL,
+	data_contratacao VARCHAR(20) NOT NULL,
+	tipo VARCHAR(15) NOT NULL,
+	local_de_trabalho VARCHAR(20) NOT NULL,
+	
+	PRIMARY KEY(id)
+
+);
