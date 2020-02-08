9b27ef3f6ffadbfcd9e7dba3cf80489a926303fa
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-02-07 21:06:40 -0300

    Adição do método DELETE

--- e-prontuario/src/main/java/com/riotinto/prontuario/controller/FuncionarioController.java
+++ e-prontuario/src/main/java/com/riotinto/prontuario/controller/FuncionarioController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -63,6 +64,21 @@ public class FuncionarioController {
 	@ResponseStatus(HttpStatus.OK)
 	public void atualizar(@Valid @RequestBody Funcionario funcionario) {
 		funcionarios.save(funcionario);
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public void deletar(@PathVariable Long id) {
+		Optional<Funcionario> funcionario = funcionarios.findById(id);
+		
+		if (funcionario.isPresent()) {
+			funcionarios.deleteById(id);	
+		
+		} else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"Não há funcionário com o ID " + id);
+		}
+		
 	}
 	
 }
