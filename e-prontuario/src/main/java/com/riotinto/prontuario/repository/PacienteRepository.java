7037af9dfb7894960adc3a62da53b8d3b9eff8a9
PedroCarl0s <pedro.carlos@dcx.ufpb.br>
2020-03-02 02:15:57 -0300

    Adição do atributo ID no método findByNomeAndSobrenome...

--- e-prontuario/src/main/java/com/riotinto/prontuario/repository/PacienteRepository.java
+++ e-prontuario/src/main/java/com/riotinto/prontuario/repository/PacienteRepository.java
@@ -8,5 +8,5 @@ import com.riotinto.prontuario.model.Paciente;
 
 public interface PacienteRepository extends JpaRepository<Paciente, Long>{
 
-	Optional<Paciente> findByNomeAndSobrenomeAndDataNascimento(String nome, String sobrenome, String dataNascimento);
+	Optional<Paciente> findByIdOrNomeAndSobrenomeAndDataNascimento(Long id, String nome, String sobrenome, String dataNascimento);
 }
